348efbd412348496238f66ea438634986c819df1
ferrisbueller66 <github email address>
2020-07-02 11:45:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(record){
+  const data = record.find( ({ result }) => result === 'W' );
+  if (data === undefined){
+    console.log(data);
+  }
+    else{
+    return data.year;  
+    }
+  
+  }
+
+// function superbowlWin(record){
+//   const data = record.find( ({ result }) => result === 'W' );
+//   return data.year;
+//   }
