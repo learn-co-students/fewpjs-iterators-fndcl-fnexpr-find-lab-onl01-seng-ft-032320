c782583ffeca4ab514a0e5b8ae09d40b24f25dac
antdp425 <antdp425@aol.com>
2020-07-03 13:30:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let found = array.find(record =>{
+    return record.result === "W"
+  })
+  if (found){
+    return found.year
+  } else {
+   return  undefined
+  }
+}
