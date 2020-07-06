bfd186652b6cc818380b1006ee3c75153ff848b4
ktalmage <ktalmage85@gmail.com>
2020-07-06 12:20:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]
+superbowlWin=(record) =>{
+  let result = record.find(record => record.result === "W")
 
-function testFunc() {
-  return "hi"
+  return !!result ? result.year : undefined
+
 }
+
