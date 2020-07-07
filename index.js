37afed733afad5a7c943212879cec488970ec8b7
Nashmeyah <nashalrekabi@gmail.com>
2020-07-07 17:23:42 -0600

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ function testFunc() {
   return "hi";
 }
 
-const record = [
-  { year: "2018", result: "L" },
-  { year: "2017", result: "W" },
-  { year: "2016", result: "N/A" },
-];
-
 function superbowlWin(array) {
-  if (array.find((s) => s.result === "W")) {
-    return array.find((s) => s.result === "W").year;
+  let search = array.find((s) => s.result === "W");
+  if (search) {
+    return search.year;
   } else {
     return undefined;
   }
